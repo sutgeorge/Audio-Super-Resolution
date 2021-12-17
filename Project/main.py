d3c2f61337a0af62acd79cf7e5e8e2615aeb6271
Calandrinon <georgesut@yahoo.com>
2021-12-17 12:15:38 +0200

    Downsample and interpolate to obtain the low-resolution input

--- Project/main.py
+++ Project/main.py
@@ -3,7 +3,9 @@ from model import create_model
 
 
 def main():
-    model = create_model(4)
+    #model = create_model(4)
+    dataset_generator = DatasetGeneratorMock()
+    dataset_generator.decimate_and_interpolate()
 
 
 main()
