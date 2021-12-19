7edc027f6ed56dfceb0ce0df704e72f55b30bfc3
Calandrinon <georgesut@yahoo.com>
2021-12-19 17:43:48 +0200

    Load the validation set properly
    The input shapes were correct, but the validation set was still just a list of filenames that had to be loaded with np.load(...)
    Lack of concentration has a price measured in time.

--- Project/training.py
+++ Project/training.py
@@ -9,27 +9,35 @@ model = create_model(NUMBER_OF_RESIDUAL_BLOCKS)
 model.summary()
 model.compile(loss='mse', optimizer='Adam', metrics=[signal_to_noise_ratio])
 (input_data_files, target_data_files), (input_validation_files, target_validation_files), _ = DatasetGenerator.split_list_of_files()
-input_data, target_data = [], []
-
+input_data, target_data, input_validation_data, target_validation_data = [], [], [], []
 
 print("Loading the .npy files...")
 
 for index in range(0, BATCH_SIZE*NUMBER_OF_BATCHES):
     input_data.append(np.load("preprocessed_dataset/low_res/" + input_data_files[index]))
     target_data.append(np.load("preprocessed_dataset/high_res/" + target_data_files[index]))
-    print("Loaded sample {}".format(index))
+    print("Loaded training sample {}".format(index))
+
+for index in range(0, 100):
+    input_validation_data.append(np.load("preprocessed_dataset/low_res/" + input_validation_files[index]))
+    target_validation_data.append(np.load("preprocessed_dataset/high_res/" + target_validation_files[index]))
+    print("Loaded validation sample {}".format(index))
 
 input_data = np.array(input_data)
 target_data = np.array(target_data)
+input_validation_data = np.array(input_validation_data)
+target_validation_data = np.array(target_validation_data)
 
 print("Some input tensor shape: {}".format(input_data[0].shape))
 print("Some target tensor shape: {}".format(target_data[0].shape))
 print("Input data: {}".format(input_data.shape))
 print("Target data: {}".format(target_data.shape))
+print("Input validation data: {}".format(input_validation_data.shape))
+print("Target validation data: {}".format(target_validation_data.shape))
 print("Training started...")
 
 model.fit(input_data, target_data,
           batch_size=BATCH_SIZE,
           epochs=1,
-          validation_data=(input_validation_files, target_validation_files),
+          validation_data=(input_validation_data, target_validation_data),
           verbose=True)
